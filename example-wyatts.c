76401c1276e17c56c6cc30f10148c54f32cb5311
Jeffrey Stern <androclus@posteo.de>
2025-12-28 00:39:23 -0800

    Improved commenting and error-handling

--- example-wyatts.c
+++ example-wyatts.c
@@ -1,24 +1,68 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <locale.h>
+
+#include <unistd.h> /* for sleep() */
+#include <locale.h> /* for setlocale() */
 #include <notcurses/notcurses.h>
 
+// Sample program originally by Wyatt Sheffield at
+//    https://wyatts.xyz/blog/writingasimpleno_2020-02-09
+// adapted/updated by Jeff Stern Dec 2025
+//
+// Draw 25 rows of 25 asterisks
+
 int main(){
-    setlocale(LC_ALL, "");
-    notcurses_options ncopt;
 
-    memset(&ncopt, 0, sizeof(ncopt));
+  // localization: some value is required by notcurses. (LC_ALL, "") is also fine.
+  // See Dank's man page at
+  // https://github.com/dankamongmen/notcurses/blob/master/doc/man/man3/notcurses.3.md
+  setlocale(LC_ALL, ".UTF8");
 
-    struct notcurses* nc = notcurses_init(&ncopt, stdout);
-    struct ncplane* stdplane = notcurses_stdplane(nc);
-    for (int i = 0; i < 25; i++){
-        for (int j = 0; j < 25; j++){
-            ncplane_putchar_yx(stdplane, i, j, '*');
-            notcurses_render(nc);
-            sleep (0.05);
-        }
+  // declare the standard structure for setting global notcurses options
+  notcurses_options ncopt;
+
+  // and allocate the memory for it
+  memset(&ncopt, 0, sizeof(ncopt));
+
+  // initialize the main notcurses object and hold a handle to it
+  struct notcurses* nc = notcurses_init(&ncopt, stdout);
+  // check nc is good
+  if(nc == NULL){
+    // Note that the terminal may not be returned to standard I/O mode
+    return EXIT_FAILURE;
+  }
+
+  // get a handle to the standard plane inside the notcurses object
+  struct ncplane* stdplane = notcurses_stdplane(nc);
+
+  // loop 25 rows and columns
+  for (int i = 0; i < 25; i++){
+    for (int j = 0; j < 25; j++){
+
+      // put an asterisk at i,j on the plane..
+      ncplane_putchar_yx(stdplane, i, j, '*');
+
+      // ..and render the change to the screen (though this could be moved to
+      // just before the sleep(2) for absolute fastest rendering)
+      notcurses_render(nc);
+
+      // sleep for 50 milliseconds after each character is drawn
+      // (I don't know why Wyatt wanted to do this. It is not
+      // necessary I think.)
+      sleep (0.05);
     }
-    sleep(2);
-    notcurses_stop(nc);
-    return 0;
+  }
+
+  // hold the picture on the screen for 2 seconds
+  sleep(2);
+
+  // shut everything down, deallocate the memory, and return
+  // the terminal to standard I/O mode
+  notcurses_stop(nc);
+  // check successful
+  if (notcurses_stop(nc)) {
+    // Note that the terminal may not be returned to standard I/O mode
+    return EXIT_FAILURE;
+  }
+
+  // if we got this far, return success
+  return EXIT_SUCCESS;
 }
