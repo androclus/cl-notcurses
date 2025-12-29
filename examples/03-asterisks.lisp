95c2df7f21e2ffb4fc1da348ba73818ee56cc43f
Jeffrey Stern <androclus@posteo.de>
2025-12-29 00:40:05 -0800

    Clean up code for SBCL strictness

--- examples/03-asterisks.lisp
+++ examples/03-asterisks.lisp
@@ -18,6 +18,10 @@
 (load "../cffi-notcurses.lisp")
 (in-package :cffi-notcurses)
 
+(defparameter *nc-handle* nil)
+(defparameter *nc-stdplane* nil)
+(defparameter *nc-opts-ptr* nil)
+
 ;;; make my beloved "while" macro
 ;;; courtesy of
 ;;; https://stackoverflow.com/questions/65304891/how-to-do-a-while-loop-in-common-lisp
@@ -35,11 +39,11 @@
      (progn
        ;; create a global pointer and allocate memory (in C space) for the
        ;; notcurses-options struct
-       (setf *nc-opts-ptr* (cffi:foreign-alloc '(:struct notcurses_options)))
+       (defparameter *nc-opts-ptr* (cffi:foreign-alloc '(:struct notcurses_options)))
 
        ;; Access and set the fields using foreign-slot-value (or with-foreign-slots)
        (setf
-        (foreign-slot-value *nc-opts-ptr* '(:struct notcurses_options) 'term) "xterm-kitty"
+        (foreign-slot-value *nc-opts-ptr* '(:struct notcurses_options) 'term) "" ;; notcurses should fill this in from env
         (foreign-slot-value *nc-opts-ptr* '(:struct notcurses_options) 'loglevel) 0
         (foreign-slot-value *nc-opts-ptr* '(:struct notcurses_options) 'margin-t) 0
         (foreign-slot-value *nc-opts-ptr* '(:struct notcurses_options) 'margin-r) 0
@@ -54,44 +58,41 @@
 ;;;(terpri)
 
        ;; Initialize notcurses, picking up the handle (pointer) to the main nc object
-       (setf *nc-handle* (notcurses-init *nc-opts-ptr* *file*))
+       (defparameter *nc-handle* (notcurses-init *nc-opts-ptr* *file*))
        
        ;; Get a stdplane
-       (setf *nc-stdplane* (notcurses-stdplane *nc-handle*))
+       (defparameter *nc-stdplane* (notcurses-stdplane *nc-handle*))
 
-       ;; y (rows): we will make 25 rows of asterisks
-       (setf y 1)
-       (setf ymax 26)
-       ;; x (columns): we will make 25 (columns of) asterisks in each row
-       (setf x 1)
-       (setf xmax 26)
 
-       ;; return value for putting a character into a cell
-       (setf pcint nil)
+       (let ((ymax 26)    ;; number of rows
+             (xmax 26)    ;; number of columns
+             (y  1)       ;; loop counter for rows
+             (x  1)       ;; loop counter for columns
+             (pcint nil)
+             (render-output nil)) ;; return value for putting a character into a cell
+         (while (< y ymax)
+                (while (< x xmax)
+                       ;; draw the asterisk at row y, column x
+                       (setq pcint (ncplane-putchar-yx *nc-stdplane* y x 42 ))
+                       ;; Register to the screen
+                       ;; If you want to try showing all the asterisk lines at once,
+                       ;; try moving the following line to just before
+                       ;; the (sleep 2) below. (For speed in that case, you may
+                       ;; also want to comment out the (sleep 0.005) line as well.)
+                       (setq render-output (notcurses-render *nc-handle*))
+                       (setq x (1+ x))
+                       ;; pause for 50 milliseconds
+                       (sleep 0.005))
+                (setq x 1) ; return to 1st column
+                (setq y (1+ y))) ; and go down to next row
 
-       (while (< y ymax)
-         (while (< x xmax)
-           ;; draw the asterisk at row y, column x
-           (setq pcint (ncplane-putchar-yx *nc-stdplane* y x 42 ))
-           ;; Register to the screen
-           ;; If you want to try showing all the asterisk lines at once,
-           ;; try moving the following line to just before
-           ;; the (sleep 2) below. (For speed in that case, you may
-           ;; also want to comment out the (sleep 0.005) line as well.)
-           (setf render-output (notcurses-render *nc-handle*))
-           (setf x (1+ x))
-           ;; pause for 50 milliseconds
-           (sleep 0.005))
-         (setf x 1) ; return to 1st column
-         (setf y (1+ y))) ; and go down to next row
+         ;; wait a couple of seconds at the end
+         (sleep 2)))
 
-       ;; wait a couple of seconds at the end
-       (sleep 2))
-
-  ;; unwind-protect cleanups:
-  ;; 1. free/release the notcurses object (and everything it contains)
-  (notcurses-stop *nc-handle*)
-  ;; 2. free the options struct
-  (foreign-free *nc-opts-ptr*)
-  ;; 3. close the stream
-  (fclose *file*))
+       ;; unwind-protect cleanups:
+       ;; 1. free/release the notcurses object (and everything it contains)
+       (notcurses-stop *nc-handle*)
+       ;; 2. free the options struct
+       (foreign-free *nc-opts-ptr*)
+       ;; 3. close the stream
+       (fclose *file*))
